64dd9f14f924c96c5f9c6ccdb2ac3f5e77a279cc
sunpro518 <2452900287@qq.com>
2017-05-01 15:42:28 +0800

    mac/sunpro/java commit first

---
+++ 20160402/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+    private static int[][] bg = new int[15][10];
+    private static int[][] brick = new int[4][4];
+    private static int po;
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        for(int i = 0; i < 15; i++){
+            for(int j = 0; j< 10; j++){
+                
+            }
+        }
+        
+    } 
+}
